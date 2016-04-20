92e00c48c190400d6f4b8b7ece04f7a111b9bde6
Vincent Barrault <vbarrault@oxent.net>
2016-04-20 15:35:30 +0200

    feat(index): add mustache and match template

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var height = 90,
+    var height = 118,
         bracket = document.getElementById('bracket'),
         body = document.body,
         html = document.documentElement,
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var updateMatchHeight = function(swiper) {
         var progress = swiper.progress * (swiper.slides.length-1);
         for (var i=0; i<swiper.slides.length; i++) {
-            var matches  = swiper.slides[i].getElementsByClassName('match');
+            var matches  = swiper.slides[i].getElementsByClassName('match-container');
             for (var j=0; j<matches.length; j++) {
                 matches[j].style.height = (height * Math.pow(2, i-progress)) + "px";
             }
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
         centeredSlides: true,
-        spaceBetween: 20,
+        spaceBetween: 10,
 
         onSliderMove: function (swiper, event) {
             // updateTop(event.touches[0]);
@@ -47,4 +47,26 @@ document.addEventListener('DOMContentLoaded', function () {
             updateMatchHeight(swiper);
         }
     });
+
+    get('views/match.html', function (template) {
+        var matchHTML = Mustache.render(template, {
+            'opponent1_name': 'Participant 1',
+            'opponent2_name': 'Participant 2',
+            'match_name': 'Match of the year'
+        });
+        var matches = document.getElementsByClassName('match-container')
+        for (var j=0; j<matches.length; j++) {
+            matches[j].innerHTML = matchHTML;
+        }
+    });
 });
+
+function get(url, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState != 4 || httpRequest.status != 200) return;
+        callback(httpRequest.responseText);
+    };
+    httpRequest.open('GET', url);
+    httpRequest.send();
+}
