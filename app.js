3132a831e6950df22fa47a0a47f6f16846b2949d
VincentBee <vincentb45@gmail.com>
2016-05-29 15:11:10 +0200

    refactor(sass): remove toornament official css, match-container height calculation in css

--- app.js
+++ app.js
@@ -1,48 +1,9 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var height = 90,
-        bracket = document.getElementById('bracket'),
-        body = document.body,
-        html = document.documentElement,
-        screenHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-    var resetMatchClasses = function(swiper) {
-        var matches = document.getElementsByClassName('match-container');
-        for (var i = 0; i < matches.length; i++) {
-            matches[i].classList.remove('size-small');
-        }
-    };
-
-    var addMatchClasses = function(swiper) {
-        var activeIndex = swiper.activeIndex;
-        for (var i=0; i<activeIndex; i++) {
-            var matches  = swiper.slides[i].getElementsByClassName('match-container');
-            for (var j=0; j<matches.length; j++) {
-                matches[j].classList.add('size-small');
-            }
-        }
-    };
-
-    var updateMatchHeight = function(swiper) {
-        var progress = swiper.progress * (swiper.slides.length-1);
-        for (var i=0; i<swiper.slides.length; i++) {
-            var matches  = swiper.slides[i].getElementsByClassName('match-container');
-            for (var j=0; j<matches.length; j++) {
-                matches[j].style.height = (height * Math.pow(2, i-progress)) + "px";
-            }
-        }
-
-        resetMatchClasses(swiper);
-        addMatchClasses(swiper);
-    };
-
     var swiper = new Swiper('.swiper-container', {
         slidesPerView: 'auto',
-        centeredSlides: true,
-        spaceBetween: 10,
-        onInit: updateMatchHeight,
-        onSlideChangeEnd: updateMatchHeight
+        centeredSlides: true
     });
 
     get('views/match.html', function (template) {
